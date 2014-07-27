eca4caa1cfcac4fb954b37f66952f6c23518924f
Anton Skornyakov <an.skornyakov@gmail.com>
2014-07-27 20:58:54 +0400

    improve programm options support add default crawler level option add positional source crawling option add short named options -h -s -l

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <concurrentqueue.h>
 #include "boost/program_options.hpp"
 
@@ -7,28 +8,43 @@ namespace po = boost::program_options;
 
 int main(int argc, char** argv)
 {
+    uint32_t crawlerLvl = 0;
+
     // Declare the supported options.
     boost::program_options::options_description desc("Allowed options");
     desc.add_options()
-        ("help", "produce help message")
-        ("level", po::value<int>(), "set crawler level")
-        ("source", po::value<int>(), "set crawler start url")
+        ("help,h", "produce help message")
+        ("level,l", po::value<uint32_t>(&crawlerLvl)->default_value(10), "set crawler level")
+        ("source,s", po::value<std::string>(), "set crawler start url")
     ;
+
+    po::positional_options_description p;
+    p.add("source", -1);
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store(po::command_line_parser(argc, argv).
+              options(desc).positional(p).run(), vm);
     po::notify(vm);
+
 
     if (vm.count("help")) {
         cout << desc << "\n";
         return 1;
     }
 
-    if (vm.count("compression")) {
-        cout << "Compression level was set to "
-     << vm["compression"].as<int>() << ".\n";
+    if (vm.count("level")) {
+        cout << "Crawler level was set to "
+     << vm["level"].as<uint32_t>() << ".\n";
     } else {
-        cout << "Compression level was not set.\n";
+        cout << "Crawler level was not set\n";
+    }
+
+    if (vm.count("source")) {
+        cout << "Crawler level was set to "
+     << vm["source"].as<std::string>() << ".\n";
+    } else {
+        cout << "Source was not set.\n";
     }
 
     cout << "Hello World!" << endl;
